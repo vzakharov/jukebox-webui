993a35bc7bae996b58c235b8400f6f506d6fce81
Vova Zakharov <vzakharov@gmail.com>
2023-09-02 14:32:36 +0300

    Update relative_imports.py

--- relative_imports.py
+++ relative_imports.py
@@ -1,6 +1,21 @@
+# Relative Import Converter
+#
+# This Python script is used to convert absolute imports to relative imports in a Python project.
+# It recursively traverses through all the Python files in the specified directory and its subdirectories.
+# For each Python file, it identifies all the import statements and modifies them from absolute to relative.
+# This is particularly useful when you want to make your Python modules/packages portable or when you are
+# restructuring your project and need to update the import paths.
+#
+# Usage:
+# Run the script from the command line with the directory path as argument.
+# Example: python relative_import_converter.py <directory_path>
+#
+# Note: This script uses the `ast` module to parse the Python source code and the `re` module for string manipulation.
+
 import os
 import re
 import ast
+import argparse
 
 def get_imports(source):
   tree = ast.parse(source)
@@ -28,4 +43,8 @@ def process_directory(path):
         relative_path = os.path.relpath(root, path).replace(os.sep, '.')
         replace_imports(os.path.join(root, file), relative_path)
 
-process_directory('.')
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description='Convert absolute imports to relative in a Python project.')
+  parser.add_argument('path', help='The directory path to process')
+  args = parser.parse_args()
+  process_directory(args.path)
