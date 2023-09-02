e5505591cdbb1b2376fdcd84f9b498a5fde9e7df
Vova Zakharov <vzakharov@gmail.com>
2023-09-02 14:47:32 +0300

    Update longfiles.py

--- longfiles.py
+++ longfiles.py
@@ -10,9 +10,18 @@ def count_relevant_lines(filename):
       count += 1
   return count
 
+file_counts = {}
 for root, dirs, files in os.walk('.'):
   for file in files:
     if file.endswith('.py'):
       filename = os.path.join(root, file)
-      if count_relevant_lines(filename) > 50:
-        print(filename)
+      count = count_relevant_lines(filename)
+      if count > 50:
+        file_counts[filename] = count
+
+# Sort the dictionary by its values in descending order
+sorted_file_counts = sorted(file_counts.items(), key=lambda x: x[1], reverse=True)
+
+# Print the filenames along with their counts
+for filename, count in sorted_file_counts:
+  print(f'{filename}: {count} lines')
